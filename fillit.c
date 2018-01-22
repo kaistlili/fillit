e8d18fc904d2e7efadc3339ec60a219af2e469d9
Kais TLILI <ktlili@e2r12p21.42.fr>
2018-01-22 14:32:09 +0100

    changed list to array of unsigned char

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: ktlili <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 15:25:12 by ktlili            #+#    #+#             */
-/*   Updated: 2018/01/18 10:04:09 by ktlili           ###   ########.fr       */
+/*   Updated: 2018/01/22 14:30:52 by ktlili           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ void ft_showtetrimino(unsigned char *tetrimino)
 		i++;
 	}
 }
+
 void	ft_shift(unsigned char *tetrimino)
 {
 	int i;
@@ -84,69 +85,50 @@ void	ft_sort(unsigned char *tetrimino)
 	
 }
 
-t_list	**ft_readfile(int ac, char **av)
+int	ft_readfile(int ac, char **av, unsigned char *tetri)
 {
 	int fd;
 	int i;
 	char buffer[20];
-	unsigned char *bitflag;
-	t_list *next;
-	t_list **tetri;
-
-	tetri = malloc(sizeof(t_list*));
+	
 	i = 0;
 	if (ac != 2)
 	{
 		ft_putstr("Invalid input.\n");
-		return (NULL);
+		return (-1);
 	}
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
 	{
 		ft_putstr("Cannot open file.\n");
-		return (NULL);
+		return (-1);
 	}
-	bitflag = ft_memalloc(4);
+	if (tetri == NULL)
+		return (-1);
 	while (read(fd, &buffer, 20) == 20)
 	{
-		ft_bzero(bitflag, 4);
-		ft_tobitflag(buffer, bitflag);
-		if (i == 0)
-		{
-			*tetri = ft_lstnew(bitflag, 4);
-			i++;
-		}
-		else
-		{
-			next = ft_lstnew(bitflag, 4);
-			ft_lstadd(tetri, next);
-			i++;
-		}
+		ft_tobitflag(buffer, &tetri[i]);
+
+		i = i + 4;
 	}
 
-	return (tetri);
+	return (i / 4);
 }
 
 int main(int argc, char **argv)
 {
-	t_list **tetri;
-	int		end;
+	unsigned char tetri[26 * 4];
+	int		size;
+	int		i;
 
-	end = 0;
-	tetri = ft_readfile(argc, argv);
-	if (tetri == NULL)
-		return (0);
-	printf("returned\n");
-	while (end < 2)
+	i = 0;
+	ft_bzero(tetri, 26*4);
+	size = ft_readfile(argc, argv, tetri);
+	while ((i / 4) < size)
 	{
-		ft_sort((*tetri)->content);
-		ft_showtetrimino((*tetri)->content);
+		ft_showtetrimino(&tetri[i]);
 		ft_putstr("\n");
-		if (end == 0)
-			*tetri = (*tetri)->next;
-		else
-			end = 2;
-		if ( (*tetri)->next == NULL && end == 0)	
-			end = 1;
+		i = i + 4;
 	}
+	
 }
