54dccd407b3e788792c7331212137b9b1810c275
Kais TLILI <ktlili@e3r3p8.42.fr>
2018-01-29 18:54:14 +0100

    commit

--- fillit.c
+++ fillit.c
@@ -6,84 +6,17 @@
 /*   By: ktlili <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 15:25:12 by ktlili            #+#    #+#             */
-/*   Updated: 2018/01/22 14:54:41 by ktlili           ###   ########.fr       */
+/*   Updated: 2018/01/26 16:46:57 by ktlili           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <fcntl.h>
 #include "libft.h"
+#include "fillit.h"
 #include <stdio.h>
 
 int ft_solver(unsigned char *tetri, int size);
-
-void	ft_swap(unsigned char *a, unsigned char *b)
-{
-	unsigned char temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-void ft_showtetrimino(unsigned char *tetrimino)
-{
-	int i;
-
-	i = 0;
-	while (i < 4)
-	{
-		ft_showbits(tetrimino[i]);
-		ft_putchar('\n');
-		i++;
-	}
-}
-
-void	ft_shift(unsigned char *tetrimino)
-{
-	int i;
-
-	i = 0;
-	while (i < 4)
-	{
-		tetrimino[i] = tetrimino[i] << 1;
-		i++;
-	}
-}
-
-void	ft_sort(unsigned char *tetrimino)
-{
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-	while (j == 0)
-	{
-		j = 1;
-		i = 0;
-		while (i < 3)
-		{
-			if (tetrimino[i] == 0 & tetrimino[i + 1] != 0)
-			{
-				ft_swap(&tetrimino[i], &tetrimino[i+1]);
-				i++;
-				j = 0;
-			}
-			else
-				i++;
-		}
-	}
-	while (j == 1)
-	{
-		if (((tetrimino[0] & 128) == 0) && ((tetrimino[1] & 128) == 0 )
-				&& ((tetrimino[2] & 128) == 0))
-			ft_shift(tetrimino);
-		else
-			j = 0;
-	}
-	
-}
 
 int	ft_readfile(int ac, char **av, unsigned char *tetri)
 {
@@ -108,7 +41,6 @@ int	ft_readfile(int ac, char **av, unsigned char *tetri)
 	while (read(fd, &buffer, 20) == 20)
 	{
 		ft_tobitflag(buffer, &tetri[i]);
-
 		i = i + 4;
 	}
 
