77e0da835403b11791dc19cc30d73c1bdc4b6e6e
kaistlili <kais@tlili.net>
2018-02-08 00:51:36 +0100

    fixed array to static on main

--- work4_07022018/fillit.c
+++ work4_07022018/fillit.c
@@ -18,28 +18,26 @@
 #include <stdio.h>
 int	ft_parse(char *buffer, unsigned char *tetrimino, int i)
 {
-	tetrimino = ft_memalloc(5);
 
 	tetrimino[0] = i + 65;
+	ft_bzero(&tetrimino[1], 4);
 	if (ft_tobitflag(buffer, &tetrimino[1]) == -1)
 		return (0);
-	ft_showtetrimino(&tetrimino[1]);
-	ft_putchar(tetrimino[0]);
 	return (1);
 
 }
 
-unsigned char **ft_readfile(char *file)
+int	ft_readfile(char *file, unsigned char tetri[26][5])
 {
 	int fd;
 	int i;
 	int valid;
 	char buffer[20];
-	unsigned char **tetri;
+	/*unsigned char **tetri;
 
-	tetri = malloc(sizeof(unsigned char*) * 26);
-	if (!(fd = open(file, O_RDONLY)) | (tetri == NULL))
-		return (NULL);
+	tetri = malloc(sizeof(unsigned char*) * 26);*/
+	if (!(fd = open(file, O_RDONLY)))
+		return (-1);
 	valid = 1;
 	i = 0;
 	while ((read(fd, &buffer, 20) == 20) & (valid))
@@ -47,39 +45,39 @@ unsigned char **ft_readfile(char *file)
 		valid = ft_parse(buffer, tetri[i], i);
 		if ((valid) & (read(fd, &buffer[0], 1)))
 			if (buffer[0] != '\n')
-				valid = 0;
-		printf("read function: i is: %d\n",i);
+				valid = 0;	
 		i++;
 	}
-	/*tetri[i] = NULL;*/
-	return (&tetri[0]);
-
+	tetri[i][0] = '0';
+	if (valid)
+		return (1);
+	return (-1);
 }
 
 
 int	main(int ac, char **av)
 {
-	unsigned char **tetri;
+	unsigned char tetri[26][5];
 	int i;
-
+	int ret;
 	if (ac != 2)
 	{
 		ft_putstr("Invalid input.\n");
 		return (0);
 	}
-	tetri = ft_readfile(av[1]);
+	ret = ft_readfile(av[1], tetri);
 	printf("returned from read.\n");
-	if (tetri == NULL)
+	if (ret == -1)
 	{
 		ft_putstr("Invalid file");
 		return (0);
 	}
 	i = 0;	
 	printf("starting loop\n");
-	while (tetri[i] != NULL)
+	while (tetri[i][0] != '0')
 	{
-/*		ft_putchar(tetri[i][0]);
-		ft_putchar('\n');*/
+		ft_putchar(tetri[i][0]);
+		ft_putchar('\n');
 		ft_showtetrimino(&tetri[i][1]);
 		ft_putchar('\n');
 		i++;
