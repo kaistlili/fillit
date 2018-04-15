6139b732f045c156a0596d7dc2a17fd837f82dc1
Kais TLILI <ktlili@e1r9p7.42.fr>
2018-04-15 07:49:16 +0200

    solver is now 24 lines removed j variable

--- current_work/ft_solver.c
+++ current_work/ft_solver.c
@@ -6,60 +6,52 @@
 /*   By: ktlili <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/22 14:39:23 by ktlili            #+#    #+#             */
-/*   Updated: 2018/04/13 08:48:46 by ktlili           ###   ########.fr       */
+/*   Updated: 2018/04/15 07:48:43 by ktlili           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "fillit.h"
-#include <stdio.h>
 
-
-int	ft_placable(unsigned short *bitmap, unsigned short *tetri, int square, int *j)
+int	ft_placable(unsigned short *bitmap, unsigned short *tetri, int square)
 {
-	if (ft_validbitmap(bitmap, tetri)) 
+	if (ft_validbitmap(bitmap, tetri))
 	{
-		ft_overlap(tetri, bitmap); 
+		ft_overlap(tetri, bitmap);
 		if (ft_square(bitmap) < square)
-			return(1);
+			return (1);
 		else
 		{
-			*j = 0;
 			ft_removetetri(tetri, bitmap);
 			return (0);
-		}	
+		}
 	}
 	else
 		return (0);
 }
 
-
-// we can ditch the J variable by breaking loop when ft_square(tetri[index]) > *square
-int solve(unsigned short tetri[28][17],int *square, int index, char solution[17][17])
+int	solve(unsigned short tetri[28][17], int *square, int index, char solution[17][17])
 {
 	int currentline;
-	int j;
-	
+
 	if (tetri[index][0] == 27)
 	{
 		if (ft_square(&tetri[26][1]) < *square)
 			ft_save_solution(tetri, square, solution);
 		return (1);
-	} 
-	j = 1;
+	}
 	currentline = 0;
 	while ((currentline < *square) && (ft_checkborder(&tetri[index][1])))
 	{
-		while ((j == 1) && (ft_checkborder(&tetri[index][1])))
+		while ((ft_square(&tetri[index][1]) < *square) && (ft_checkborder(&tetri[index][1])))
 		{
-			if ((ft_placable(&tetri[26][1], &tetri[index][1], *square, &j)> 0))
+			if ((ft_placable(&tetri[26][1], &tetri[index][1], *square) > 0))
 			{
 				solve(tetri, square, index + 1, solution);
 				ft_removetetri(&tetri[index][1], &tetri[26][1]);
 			}
 			ft_shiftright(&tetri[index][1]);
 		}
-		j = 1;
 		ft_sortnextline(&tetri[index][1]);
 		currentline++;
 	}
